02900bec2b16e95bfcf09fdef36bad179ce9ccb6
Swapna Varma <swapna.varma@yahoo.com>
2017-11-23 22:52:55 -0500

    updates

--- frontend/src/components/NotFoundPage.js
+++ frontend/src/components/NotFoundPage.js
@@ -1,18 +1,20 @@
 
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { Header, Button } from "semantic-ui-react";
+import { Header, Button,Container } from "semantic-ui-react";
 
 
 class NotFoundPage extends Component{
 	render() {
 		return (
+			<Container textAlign="left">
             <div className="w3-card-4">
             <Header as="h3" dividing color="red" textAlign="center">
               Page Not found
             </Header>
 				<Button><Link to="/">Back to Home</Link></Button>
 			</div>
+			</Container>
 		)
 	}
 }
